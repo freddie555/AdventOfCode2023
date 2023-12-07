68c0ca2f086ad690283f724d30dec8ac7a486ebe
Freddie <freddie.freeston@accesspay.com>
2023-12-07 12:27:03 +0000

    Add project files.

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var aocData = File.ReadAllLines("C:\\Users\\freddie.freeston\\source\\repos\\AdventOfCode\\AdventOfCode\\aoc_data.txt");
+List<int> lineTotals = new List<int>();
+List<string> digitArray = new List<string> { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+var digitAsWordList = new List<string> { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+digitArray.AddRange(digitAsWordList);
+for (int i = 0; i < aocData.Length; i++)
+{
+    KeyValuePair<int, string> firstDigit = new KeyValuePair<int, string>(-1, "N/A");
+    KeyValuePair<int, string> lastDigit = new KeyValuePair<int, string>(-1, "N/A");
+    digitArray.ForEach(x =>
+    {
+        var indexOfDigit = aocData[i].IndexOf(x);
+        if (indexOfDigit >= 0)
+        {
+            if (firstDigit.Key == -1 || indexOfDigit < firstDigit.Key)
+            {
+                string intValue = x;
+                if (x.Length > 1)
+                {
+                    intValue = digitArray[digitAsWordList.IndexOf(x)];
+                }
+                firstDigit = new KeyValuePair<int, string>(indexOfDigit, intValue);
+            }
+        }
+
+        var lastIndexOfDigit = aocData[i].LastIndexOf(x);
+        if (lastIndexOfDigit >= 0)
+        {
+            if (lastDigit.Key == -1 || lastIndexOfDigit > lastDigit.Key)
+            {
+                string intValue = x;
+                if (x.Length > 1)
+                {
+                    intValue = digitArray[digitAsWordList.IndexOf(x)];
+                }
+                lastDigit = new KeyValuePair<int, string>(lastIndexOfDigit, intValue);
+            }
+        }
+    });
+
+    var calibrationValue = $"{firstDigit.Value}{lastDigit.Value}";
+    lineTotals.Add(Convert.ToInt32(calibrationValue));
+}
+
+var total = lineTotals.Sum();
+Console.WriteLine(total);
